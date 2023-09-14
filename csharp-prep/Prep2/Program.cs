31e6ae6a277c10d45174e5b3fad026a81ab589f0
kaitlyn-myers8208 <kaitlyn.myers8208@gmail.com>
2023-09-14 12:37:03 -0600

    Started Prep 2 Lab

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        float numPercentage = int.Parse(percentage);
+
+        if (numPercentage >= 90)
+        {
+            Console.WriteLine("Your grade is an A");
+        }
+        else if (numPercentage >= 80 && numPercentage < 90)
+        {
+            Console.WriteLine("Your grade is a B");
+        }
+        else if (numPercentage >= 70 && numPercentage < 80)
+        {
+            Console.WriteLine("Your grade is a C");
+        }
+        else if (numPercentage >= 60 && numPercentage < 70)
+        {
+            Console.WriteLine("Your grade is a D");
+        }
+        else if (numPercentage < 60)
+        {
+            Console.WriteLine("Your grade is an F");
+        }
+
     }
 }
