9f43b2f53278207ad586db856912762f6ea18d7c
kaitlyn-myers8208 <kaitlyn.myers8208@gmail.com>
2023-09-14 13:09:56 -0600

    Finished Prep 2 program.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,26 +8,86 @@ class Program
         string percentage = Console.ReadLine();
         float numPercentage = int.Parse(percentage);
 
+        string letter = "q";
+        string sign = "";
+
         if (numPercentage >= 90)
         {
-            Console.WriteLine("Your grade is an A");
+            if (numPercentage % 10 < 3)
+            {
+                sign = "-";
+            }
+            letter = "A";
         }
         else if (numPercentage >= 80 && numPercentage < 90)
         {
-            Console.WriteLine("Your grade is a B");
+            if (numPercentage % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (numPercentage % 10 < 3)
+            {
+                sign = "-";
+            }
+            letter = "B";
         }
         else if (numPercentage >= 70 && numPercentage < 80)
         {
-            Console.WriteLine("Your grade is a C");
+            if (numPercentage % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (numPercentage % 10 < 3)
+            {
+                sign = "-";
+            }
+            letter = "C";
         }
         else if (numPercentage >= 60 && numPercentage < 70)
         {
-            Console.WriteLine("Your grade is a D");
+            if (numPercentage % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (numPercentage % 10 < 3)
+            {
+                sign = "-";
+            }
+            letter = "D";
         }
         else if (numPercentage < 60)
         {
-            Console.WriteLine("Your grade is an F");
+            letter = "F";
         }
+        else
+        {
+            Console.WriteLine("That isn't a correct input");
+            Environment.Exit(0);
+        }
+
+        if (letter == "A" || letter == "F")
+        {
+            Console.WriteLine($"Your grade is an {letter}{sign}");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is a {letter}{sign}");
+        }
+
+        if (numPercentage >= 70)
+        {
+            if (numPercentage == 70)
+            {
+                Console.WriteLine("C's get degrees");
+            }
+
+            Console.WriteLine("Congrats! You passed the class!");
+        }
+        else if (numPercentage < 70)
+        {
+            Console.WriteLine("Whelp, there's always next semester...");
+        }
+
 
     }
 }
