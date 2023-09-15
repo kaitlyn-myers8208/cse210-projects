e8012018a6b511a757929152177885d75d7bf83e
kaitlyn-myers8208 <kaitlyn.myers8208@gmail.com>
2023-09-15 00:00:13 -0600

    Added the option to replay the game.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,32 +9,42 @@ class Program
         // string num = Console.ReadLine();
         // int magicNum = int.Parse(num);
 
-        Random randomGenerator = new Random();
-        int magicNum = randomGenerator.Next(1,11);
+        string userResponse = "No";
 
-        Console.WriteLine("What is your guess? ");
-        string guess = Console.ReadLine();
-        int userGuess = int.Parse(guess);
-
-        int numOfGuesses = 1;
-
-        while (userGuess != magicNum)
+        do
         {
-            if (userGuess > magicNum)
-            {
-                Console.WriteLine("Lower");
-            }
-            else if (userGuess < magicNum)
-            {
-                Console.WriteLine("Higher");
-            }
+
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1,11);
+
             Console.WriteLine("What is your guess? ");
-            guess = Console.ReadLine();
-            userGuess = int.Parse(guess);
-            numOfGuesses++;
-        }
-        
-        Console.WriteLine($"You guess it! It took you {numOfGuesses} guesses.");
+            string guess = Console.ReadLine();
+            int userGuess = int.Parse(guess);
+
+            int numOfGuesses = 1;
+
+            while (userGuess != magicNum)
+            {
+                if (userGuess > magicNum)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userGuess < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                Console.WriteLine("What is your guess? ");
+                guess = Console.ReadLine();
+                userGuess = int.Parse(guess);
+                numOfGuesses++;
+            }
+            
+            Console.WriteLine($"You guess it! It took you {numOfGuesses} guesses.");
+
+            Console.WriteLine("Do you want to play again? ");
+            userResponse = Console.ReadLine();
+
+        } while (userResponse == "Yes");
 
 
     }
