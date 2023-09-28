81086b6c71de3cb16dfa3fc9e2725c2cf1c896b5
kaitlyn-myers8208 <kaitlyn.myers8208@gmail.com>
2023-09-28 13:24:43 -0600

    Updated names of variables

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,29 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2024;
-        job1._endYear = 2032;
+        job1.JobTitle = "Software Engineer";
+        job1.Company = "Microsoft";
+        job1.StartYear = 2024;
+        job1.EndYear = 2032;
 
-        // Console.WriteLine ($"{job1._company}");
+        // Console.WriteLine ($"{job1.Company}");
         // job1.DisplayJobInfo();
 
         Job job2 = new Job();
-        job2._jobTitle = "Ice Cream Tester";
-        job2._company = "Ben and Jerry's";
-        job2._startYear = 2030;
-        job2._endYear = 2034;
+        job2.JobTitle = "Ice Cream Tester";
+        job2.Company = "Ben and Jerry's";
+        job2.StartYear = 2030;
+        job2.EndYear = 2034;
 
-        // Console.WriteLine ($"{job2._company}");
+        // Console.WriteLine ($"{job2.Company}");
         // job2.DisplayJobInfo();
 
         Resume resume1 = new Resume();
-        resume1._name = "Allison Rose";
-        resume1._jobs.Add(job1);
-        resume1._jobs.Add(job2);
+        resume1.Name = "Allison Rose";
+        resume1.Jobs.Add(job1);
+        resume1.Jobs.Add(job2);
 
-        // Console.WriteLine($"{resume1._jobs[0]._jobTitle}");
+        // Console.WriteLine($"{resume1._jobs[0].JobTitle}");
 
         resume1.DisplayResume();
 
--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,7 +1,7 @@
 public class Resume
 {
-    public string _name;
-    public List<Job> _jobs = new List<Job>();
+    public string Name;
+    public List<Job> Jobs = new List<Job>();
 
     public Resume()
     {
@@ -9,9 +9,9 @@ public class Resume
 
     public void DisplayResume()
     {
-        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Name: {Name}");
         Console.WriteLine("Jobs: ");
-        foreach (Job j in _jobs)
+        foreach (Job j in Jobs)
         {
             j.DisplayJobInfo();
         }
