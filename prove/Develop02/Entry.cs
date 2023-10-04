dd7a09527f525db172809f4f705e59dd80fddd15
kaitlyn-myers8208 <kaitlyn.myers8208@gmail.com>
2023-10-04 17:50:22 -0600

    Added basic layout for journal classes

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,16 @@
+public class Entry
+{
+    public string UserResponse;
+    public string Prompt;
+    public string Date;
+
+    public Entry()
+    {
+
+    }
+
+    public void GivePrompt()
+    {
+        
+    }
+}
---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,24 @@
+public class Journal
+{
+    public List<Entry> JournalEntries;
+    public string FileName;
+    public string UserOption;
+
+    public Journal()
+    {
+
+    }
+
+    public void SaveJournal()
+    {
+
+    }
+    public void LoadJournal()
+    {
+
+    }
+    public void ListEntries()
+    {
+
+    }
+}
