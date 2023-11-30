f4644b892bb40cb2fc5dd3590b1b585847323718
kaitlyn-myers8208 <kaitlyn.myers8208@gmail.com>
2023-11-30 13:38:40 -0700

    Finished program

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,18 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Activity> activities =  new List<Activity>();
+        // List<Activity> activities =  new List<Activity>();
         
         Running running = new Running("03 Nov 2022", 30, 4.8);
-        Console.WriteLine(running.DisplaySummary());
-        Console.WriteLine();
+        // Console.WriteLine(running.DisplaySummary());
+        // Console.WriteLine();
         
         Cycling cycling = new Cycling("03 Nov 2022", 30, 6.3);
-        Console.WriteLine(cycling.DisplaySummary());
-        Console.WriteLine();
+        // Console.WriteLine(cycling.DisplaySummary());
+        // Console.WriteLine();
 
         Swimming swimming = new Swimming("03 Nov 2022", 30, 5);
-        Console.WriteLine(swimming.DisplaySummary());
-        Console.WriteLine();
+        // Console.WriteLine(swimming.DisplaySummary());
+        // Console.WriteLine();
+
+        List<Activity> activities = new List<Activity>()
+        {
+            running, cycling, swimming
+        };
+
+        foreach (Activity a in activities)
+        {
+            Console.WriteLine(a.DisplaySummary());
+            Console.WriteLine();
+        }
     }
 }
